e4557f8e2d7d1fd294664cc315486a4ea7891b79
NomanShahid <noman.shahid@regbits.com>
2020-12-30 18:44:24 +0500

    JS in JSX

--- src/index.js
+++ src/index.js
@@ -1,45 +1,31 @@
 import React from "react";
 import ReactDom from "react-dom";
+
+// CSS
 import "./index.css";
 function BookList() {
   return (
     <section className="booklist">
       <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
     </section>
   );
 }
+
+const author = 'V. E. Schwab'
+
 const Book = () => {
+   const title = 'The Vanishing Half: A Novel The Vanishing Half: A Novel'
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/91Ql48Y0mqL._AC_UL200_SR200,200_.jpg"
+        alt=""
+      />
+      <h1>{title}</h1>
+      <h4>{author.toUpperCase()}</h4>
+      {/* JS in JSX must return something otherwise it gives error */}
+      <p>{6+6}</p>
     </article>
   );
 };
-
-const Image = () => (
-  <img
-    src="https://images-na.ssl-images-amazon.com/images/I/91Ql48Y0mqL._AC_UL200_SR200,200_.jpg"
-    alt=""
-  />
-);
-
-const Title = () => (
-  <h1>The Vanishing Half: A Novel The Vanishing Half: A Novel</h1>
-);
-
-const Author = () => (
-  <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>
-    V. E. Schwab
-  </h4>
-);
 ReactDom.render(<BookList />, document.getElementById("root"));
